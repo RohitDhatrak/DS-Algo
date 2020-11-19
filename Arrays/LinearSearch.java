67f79f3fd75696dfd34e245cb1dd04fa865c88da
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-19 22:32:57 +0530

    Update LinearSearch.java

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,7 +1,6 @@
 // We compare each element in the array with the key
 // Time Complexity O(n)
 // Space Complexity O(1)
-
 public static int linearSearch(int arr[], int key){
     for(int i = 0; i < arr.length; i++)
         if(arr[i] == key)
