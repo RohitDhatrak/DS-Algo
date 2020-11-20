36c3037917e389c1d6e4e1ab6a9bf6cb13362879
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-20 13:16:38 +0530

    Create SelectionSort.java

---
+++ Arrays/SelectionSort.java
@@ -0,0 +1,16 @@
+// In each iteration we select the minimum element from the unsorted part of the array and replace it with the first unsorted element
+
+// Time Complexity O(n^2)
+// Space Complexity O(1)
+public static void SelectionSort(int [] arr){
+    for(int i = 0; i < arr.length - 1; i++){
+        int minIndx = i;
+        for(int j = i + 1; j < arr.length; j++){
+            if(arr[minIndx] > arr[j])
+                minIndx = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndx];
+        arr[minIndx] = temp; 
+    }
+}
