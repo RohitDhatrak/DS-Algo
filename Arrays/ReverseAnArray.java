dd4ae266a9ecc69b93b29c9b6f227b8572dbc3c5
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-20 13:14:17 +0530

    Update ReverseAnArray.java

--- Arrays/ReverseAnArray.java
+++ Arrays/ReverseAnArray.java
@@ -1,6 +1,5 @@
 // We use two pointers and keep swapping the values
 
-// Method 1 - Iterative Method
 // Time Complexity O(n)
 // Space Complexity O(1)
 public static void reverseArray(int arr[]) {
@@ -9,16 +8,4 @@ public static void reverseArray(int arr[]) {
         arr[i] = arr[j];
         arr[j] = temp;
     }
-}
-    
-// Method 2 - Using a Recursive Fuction
-// Time Complexity O(n)
-// Space Complexity O(1)
-public static void reverseArray(int arr[], int i, int j) {       
-    if (i>=j)
-        return;
-    reverseArray(arr, i + 1, j - 1);
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
 }
