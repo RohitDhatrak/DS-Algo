cdcf542ed8fb5bf0aca2c65887c3de50ddbdc62b
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-21 19:25:39 +0530

    Create InsertionSort.java

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,15 @@
+// We take an element and insert it in the right place
+
+// Time Complexity O(n^2)
+// Space Complexity O(1)
+public static void insertionSort(int [] arr){
+    for(int i = 1; i < arr.length; i++){
+        int current = arr[i];
+        int j = i - 1;
+        while(arr[j] > current && j >= 0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+}
