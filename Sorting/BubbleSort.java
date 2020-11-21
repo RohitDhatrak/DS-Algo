ae50844f65aa266cb6226bf5de21b982db1e77f2
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-21 10:08:45 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,14 @@
+// In each iteration we compare the i and i + 1 element and swap them if they are in wrong order
+// Time Complexity O(n^2)
+// Space Complexity O(1)
+public static void bubbleSort(int [] arr){
+    for(int i = 1; i < arr.length; i++) {
+        for(int j = 0; j < arr.length - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
