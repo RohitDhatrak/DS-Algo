b06a1563e7173d43cf94e73f03cff156c17ce440
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-27 18:57:07 +0530

    Update Multiplication.java

--- 2dArray/Multiplication.java
+++ 2dArray/Multiplication.java
@@ -2,6 +2,9 @@
 // int[][] arr2 = new int[n2][n3];
 // int[][] ans = new int[n1][n3];
 
+// Time Complexity O(n^3)
+// Space Complexity O(1)
+
 public static int[][] multiplication(int[][] arr1, int[][] arr2, int[][] ans, int n1, int n2, int n3){
     for(int i = 0; i < n1; i++){
         for(int j = 0; j < n3; j++){
