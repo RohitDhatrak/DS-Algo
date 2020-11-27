c652c9957d8c976ee5c40c8967c1d1a4bb47a77c
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-27 09:07:55 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,9 +2,11 @@
 
 // Time Complexity O(n^2)
 // Space Complexity O(1)
+// In-place, not stable, not adaptive, not online
+
 public static void bubbleSort(int [] arr){
-    for(int i = 1; i < arr.length; i++) {
-        for(int j = 0; j < arr.length - i; j++) {
+    for(int i = 0; i < arr.length - 1; i++) {
+        for(int j = 0; j < arr.length - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
