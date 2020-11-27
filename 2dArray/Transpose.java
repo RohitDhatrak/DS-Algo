baf35d2aba33741dde6557b3b65f5cdbcc5bac7c
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-27 18:45:19 +0530

    Update Transpose.java

--- 2dArray/Transpose.java
+++ 2dArray/Transpose.java
@@ -1,3 +1,6 @@
+// Time Complexity O(n^2)
+// Space Complexity O(1)
+
 public static void transpose(int[][] arr, int row, int column){
     for(int i = 0; i < row; i++){
         for(int j = i; j < column; j++){
