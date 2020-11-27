5cab35b01e4941b3d2fd1d09c81b2e72a51ed463
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-27 18:30:41 +0530

    Update Transpose.java

--- 2dArray/Transpose.java
+++ 2dArray/Transpose.java
@@ -1,7 +1,9 @@
-for(int i = 0; i < row; i++){
-    for(int j = i; j < column; j++){
-        int temp = arr[i][j];
-        arr[i][j] = arr[j][i];
-        arr[j][i] = temp;
+public static void transpose(int[] arr, int row, int column){
+    for(int i = 0; i < row; i++){
+        for(int j = i; j < column; j++){
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
     }
 }
