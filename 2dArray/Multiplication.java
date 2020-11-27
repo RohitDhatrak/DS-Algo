53355920158533362682e452cc3b36409a8b1ebd
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-27 18:36:57 +0530

    Update Multiplication.java

--- 2dArray/Multiplication.java
+++ 2dArray/Multiplication.java
@@ -2,10 +2,13 @@
 // int[][] arr2 = new int[n2][n3];
 // int[][] ans = new int[n1][n3];
 
-for(int i = 0; i < n1; i++){
-    for(int j = 0; j < n3; j++){
-        for(int k = 0; k < n2; k++){
-            ans[i][j] += arr1[i][k] * arr2[k][j];
+public static int[][] multiplication(int[][] arr1, int[][] arr2, int[][] ans, int n1, int n2, int n3){
+    for(int i = 0; i < n1; i++){
+        for(int j = 0; j < n3; j++){
+            for(int k = 0; k < n2; k++){
+                ans[i][j] += arr1[i][k] * arr2[k][j];
+            }
         }
     }
+    return ans;
 }
