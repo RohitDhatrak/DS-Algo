317a4a24c41945b20995d6dccf5ad24b84cee617
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-11-27 18:41:48 +0530

    Update Searching.java

--- 2dArray/Searching.java
+++ 2dArray/Searching.java
@@ -1,18 +1,20 @@
 // Searching an element in a sorted 2d array
 // We start from top right corner if the element is greater than the key we reduce the column and if it is lesser than the key we increment the row
 
-boolean flag = false;
-int n = 0;
-int m = column - 1;
+public static void search(int[][] arr,int key, int row, int column){
+    int n = 0;
+    int m = column - 1;
+    boolean flag = false;
 
-while(n < row && m > 0){
-    if(key == arr[n][m]){
-        System.out.println("Element Found");
-    }else
-        if(arr[n][m] > key)
-            m--;
-        else
-            n++;
-    if(!flag)
-        System.out.println("Element not Found");
+    while(n < row && m > 0){
+        if(key == arr[n][m]){
+            System.out.println("Element Found");
+        }else
+            if(arr[n][m] > key)
+                m--;
+            else
+                n++;
+        if(!flag)
+            System.out.println("Element not Found");
+    }
 }
