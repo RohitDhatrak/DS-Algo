1160dab5e64f0e55d9243d84a4da7cd5cf4e3b8d
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-23 20:44:19 +0530

    Create Fibonacci.java

---
+++ BasicProblems/Fibonacci.java
@@ -0,0 +1,18 @@
+// we shift a and b with each iteration and keep printing a.
+
+// 0  1  1  2  3  5  8
+// a  b
+//    a  b
+//       a  b
+
+
+public static void printFibonacci(int n) {
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < n; i++) {
+        System.out.print(a + " ");
+        int nextNum = a + b;
+        a = b;
+        b = nextNum;
+    }
+}
