4806d7d78f5f7887f494c52b62ab44b70de2e453
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-25 15:13:32 +0530

    Create InverseOfANumber.java

---
+++ BasicProblems/InverseOfANumber.java
@@ -0,0 +1,6 @@
+// Question
+// if the number is 5 digits long, it'll contain all the digits from 1 to 5 without missing and repeating any digit from 1 to 5
+// If we have a number 426135 here we can see that 5 is in the 1st position and 3 is in the 2nd position
+// To calculate inverse of a number we'll put 1 in the 5th place and 2 in the 3rd place doing so for all the digits
+// so the inverse would be 416253
+
