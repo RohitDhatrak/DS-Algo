76b155bb5815ce586ce8239bd9fd22035da45afd
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-25 14:51:31 +0530

    Update CountDigits.java

--- BasicProblems/CountDigits.java
+++ BasicProblems/CountDigits.java
@@ -1,5 +1,8 @@
 // Count the number of digits in a number
 
+// Time Complexity  O(n)
+// Space Complexity O(1)
+
 public static int countDigits(int num) {
     int count = 0;
     while (num != 0) {
