10b12dc7063c57106094630c76ecc192d953446b
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-25 14:43:35 +0530

    Create ReverseANumber.java

---
+++ BasicProblems/ReverseANumber.java
@@ -0,0 +1,16 @@
+public static int reverseNumber(int num) {
+    int count = 0;
+    int temp = num;
+    int ans = 0;
+    while (temp != 0) {
+        temp /= 10;
+        count++;
+    }
+    while (count != 0) {
+        int digit = num % 10;
+        num /= 10;
+        ans += digit * Math.pow(10, count - 1);
+        count--;
+    }
+    return ans;
+}
