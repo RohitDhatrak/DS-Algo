b82cd646b6294dd95ff34fa7529028d2a27b4b0f
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-28 12:05:43 +0530

    Create Gcd&Lcm.java

---
+++ BasicProblems/Gcd&Lcm.java
@@ -0,0 +1,24 @@
+// Calculate GCD and LCM of a and b
+
+Time Complexity  O( )
+Space Complexity O(1)
+
+
+// Eulclids Algorithm for GCD
+
+// a )▔▔
+//     b
+//    ▔▔
+//    b % a)▔▔      ie the remainder (b%a) will  be the new a
+//            a       and a will be the new b
+//           ▔▔
+//              .......
+
+
+public static int gcd(int a, int b) {
+    if(a== 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+// a * b = GCD * LCM   >> so LCM can be found after finding GCD
