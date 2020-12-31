861113a11d9c348cdb5549257d784909216765d7
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-31 14:58:34 +0530

    Update DivisibleBy2.java

--- Bit Manipulation/DivisibleBy2.java
+++ Bit Manipulation/DivisibleBy2.java
@@ -1,7 +1,7 @@
 // check if the number is divisible by 2
 
 public static boolean powerOf2(int num) {
-    int ans = (num & 1);
+    int ans = (num & 1);   // we check if the last bit is zero
     if(ans == 0)
         return true;
     else
