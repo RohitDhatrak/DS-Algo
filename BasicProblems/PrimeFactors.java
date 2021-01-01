00ce052133acb4b06fab1f4eaef7077f5cf9a5d5
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-01 13:40:23 +0530

    Create PrimeFactors.java

---
+++ BasicProblems/PrimeFactors.java
@@ -0,0 +1,18 @@
+// print the prime factors of a number
+// eg  n = 1440  >> 2 2 2 2 2 3 3 5
+
+public static void primeFactors(int n) {
+    while (n != 1) {
+        for (int i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                n /= i;
+                break;
+            }
+            if (i == Math.floor(Math.sqrt(n))) {
+                System.out.print(n + " ");
+                n /= n;
+            }
+        }
+    }
+}
