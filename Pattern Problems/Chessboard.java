72434cef04401064c1431a2423c917153fa2b62f
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-02 23:55:22 +0530

    Update Chessboard.java

--- Pattern Problems/Chessboard.java
+++ Pattern Problems/Chessboard.java
@@ -9,7 +9,7 @@
 # # # #
 */
 
-public static void triangle(int n) {
+public static void chessboard(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i % 2 == 0 && j % 2 != 0)
