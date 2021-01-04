ac00c9172869f9db7d7173cdf94d9232e7f8ea2a
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-04 23:48:15 +0530

    Update FindUnique.java

--- Bit Manipulation/FindUnique.java
+++ Bit Manipulation/FindUnique.java
@@ -1,4 +1,4 @@
-// Find the unique number in the array
+// Find the unique number in the array (every other number occurs twice)
 
 public static int findUnique(int[] arr) {
     int num = 0;
@@ -8,7 +8,7 @@ public static int findUnique(int[] arr) {
     return num;
 }
 
-// find two unique nums from an array
+// Find two unique numbers from an array (every other number occurs twice)
 
 public static int[] find2Unique(int[] arr) {
     int diff = 0;
@@ -24,4 +24,21 @@ public static int[] find2Unique(int[] arr) {
     arr[0] = oneNo; 
     arr[1] = oneNo ^ diff;                     // by applying xor to the number and the diff we get the second unique number
     return arr;
+}
+
+// Find one unique numbers from an array (every other number occurs thrice)
+
+public static int findUnique3(int[] arr) {
+    int ans = 0;
+    for (int i = 0; i < 64; i++) {                   // we count the no of set bits in all 64 places
+        int count = 0;
+        for (int j = 0; j < arr.length; j++) {
+            if ((arr[j] & 1 << i) > 0)
+                count++;
+        }
+        if (count % 3 != 0) {                        // the count will be divisible by 3 if the number is repeated thrice
+            ans = ans | 1 << i;
+        }
+    }
+    return ans;
 }
