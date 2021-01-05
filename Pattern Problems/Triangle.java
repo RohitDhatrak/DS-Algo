df5bf6963433b8b17b380714c605b8f1b4a69c73
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-05 23:53:44 +0530

    Update Triangle.java

--- Pattern Problems/Triangle.java
+++ Pattern Problems/Triangle.java
@@ -14,3 +14,24 @@ public static void triangle(int n) {
         System.out.println();
     }
 }
+
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+public static void pattern(int n) {
+    for (int i = 5; i > 0; i--) {
+        for (int j = 0; j < i - 1; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 0; j <= 5 - i; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
