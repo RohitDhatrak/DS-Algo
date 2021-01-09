15924c80d63121f60549520d31507d836c32c54e
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-09 17:04:05 +0530

    Update PrintDigits.java

--- BasicProblems/PrintDigits.java
+++ BasicProblems/PrintDigits.java
@@ -3,17 +3,18 @@
 // Time Complexity  O(log(n))
 // Space Complexity O(1)
 
-public static void printDigits(int num) {
-    int count = 0;
-    int temp = num;
+function printDigits(num) {
+    let count = 0;
+    let temp = num;
     while (temp != 0) {
-        temp /= 10;
+        temp = Math.floor(temp / 10);
         count++;
     }
-    while (count != 0) {    // A number like 3000 will be an edge case if we check for num != 0
-        int digit = num / (int)Math.pow(10, count - 1);
+    while (count != 0) {
+        // A number like 3000 will be an edge case if we check for num != 0
+        let digit = Math.floor(num / Math.pow(10, count - 1));
         num %= Math.pow(10, count - 1);
         count--;
-        System.out.println(digit);
+        console.log(digit);
     }
 }
