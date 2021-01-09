96b763981ce90f90842c9ec2b733f0cdc7ff2951
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-10 01:18:36 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -4,11 +4,11 @@
 // Space Complexity O(1)
 // In-place, Stable, Adaptive, Not Online
 
-public static void bubbleSort(int [] arr){
-    for(int i = 0; i < arr.length - 1; i++) {
-        for(int j = 0; j < arr.length - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                int temp = arr[j];
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
