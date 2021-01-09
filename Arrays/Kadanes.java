06cc6fb8b8105d8bb46f4e8d8ad0ba204fad5f02
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-10 01:09:53 +0530

    Update Kadanes.java

--- Arrays/Kadanes.java
+++ Arrays/Kadanes.java
@@ -6,14 +6,14 @@
 // Time Complexity O(n)
 // Space Complexity O(1)
 
-public static int kadanes(int[] arr) {
-    int currentSum = 0;
-    int maxSum = Integer.MIN_VALUE;
-    for(int i = 0; i < arr.length; i++){
-        if(currentSum < 0)
+function kadanes(arr) {
+    let currentSum = 0;
+    let maxSum = Number.MIN_VALUE;
+    for (let i = 0; i < arr.length; i++) {
+        if (currentSum < 0) 
             currentSum = 0;
         currentSum += arr[i];
-        if(currentSum > maxSum)
+        if (currentSum > maxSum) 
             maxSum = currentSum;
     }
     return maxSum;
