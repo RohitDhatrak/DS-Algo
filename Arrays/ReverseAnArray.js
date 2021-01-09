ec52883152340ddc5bd666e3f19b321a37759826
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-10 01:11:50 +0530

    Update and rename ReverseAnArray.java to ReverseAnArray.js

--- Arrays/ReverseAnArray.js
+++ Arrays/ReverseAnArray.js
@@ -3,10 +3,11 @@
 // Time Complexity O(n)
 // Space Complexity O(1)
 
-public static void reverseArray(int arr[]) {
-    for (int i = 0, j = arr.length - 1; i < j; i++, j--) {   
-        int temp = arr[i];
+function reverseArray(arr) {
+    for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+        let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    return arr;
 }
