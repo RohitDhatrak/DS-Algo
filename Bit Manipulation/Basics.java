59afd8480ec0512fa6ecae2710bb6fe2c737041b
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-09 23:46:07 +0530

    Update Basics.java

--- Bit Manipulation/Basics.java
+++ Bit Manipulation/Basics.java
@@ -1,20 +1,29 @@
 // Basic operations like get bit, set bit, clear bit & update bit
 
-public static int getBit(int n, int pos) {  // get the bit at pos
-    return n & (1 << pos);
+function getBit(n, pos) {
+    // get the bit at pos
+    console.log("log" + (n & (1 << pos)));
+    if (n & (1 << pos)) 
+        return 1;
+    else 
+        return 0;
 }
 
-public static int setBit(int n, int pos) {  // set the bit at pos
+function setBit(n, pos) {
+    // set the bit at pos
     return n | (1 << pos);
 }
 
-public static int clearBit(int n, int pos) {  // clear the bit at pos
-    return n & (~(1 << pos));
+function clearBit(n, pos) {
+    // clear the bit at pos
+    return n & ~(1 << pos);
 }
 
-public static int updateBit(int n, int pos, int val) { // update the bit at pos with give val
-    if (val == 1)
+function updateBit(n, pos, val) {
+    // update the bit at pos with give val
+    if (val == 1) 
         return setBit(n, pos);
-    else
+    else 
         return clearBit(n, pos);
 }
+
