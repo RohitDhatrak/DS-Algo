73607dd9a20bd491db93f143cdc5d24249d99810
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-10 00:08:25 +0530

    Update and rename NoOf1s.java to NoOf1s.js

--- Bit Manipulation/NoOf1s.js
+++ Bit Manipulation/NoOf1s.js
@@ -1,7 +1,7 @@
 // count the no of ones in the binary representation of the number
 
-public static int NoOf1s(int num) {
-    int count = 0;
+function noOf1s(num) {
+    let count = 0;
     while (num > 0) {
         num = num & (num - 1);
         count++;
