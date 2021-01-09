11f19e498cbc44197d9c791a50ef0151dd5ff3fa
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-09 23:25:00 +0530

    Update SieveOfEratosthenes.java

--- BasicProblems/SieveOfEratosthenes.java
+++ BasicProblems/SieveOfEratosthenes.java
@@ -3,16 +3,17 @@
 // Time Complexity  O(n*log(log(n)))
 // Space Complexity O(n)
 
-public static int[] sieve(int n) {
-    int[] arr = new int[n - 1];
-    for(int i = 2; i <= n; i++)
-        arr[i - 2] = i;
+function sieve(n) {
+    let arr = [];
+    for (let i = 2; i <= n; i++) arr[i - 2] = i;
 
-    for (int i = 2; i*i <= n; i++) {     // we stop if the square of i is greater than n
-        if (arr[i - 2] != 0) {          // if the number is marked then all it's factors are marked so we move to the next number
-            for (int j = (int) Math.pow(i, 2); j <= n; j++) {  // we start checking from i^2 because all the previous factors of i would be already marked
-                if (arr[j - 2] % i == 0)
-                    arr[j - 2] = 0;
+    for (let i = 2; i * i <= n; i++) {
+        // we stop if the square of i is greater than n
+        if (arr[i - 2] != 0) {
+            // if the number is marked then all it's factors are marked so we move to the next number
+            for (let j = Math.pow(i, 2); j <= n; j++) {
+                // we start checking from i^2 because all the previous factors of i would be already marked
+                if (arr[j - 2] % i == 0) arr[j - 2] = 0;
             }
         }
     }
