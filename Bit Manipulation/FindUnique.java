2ac352f9866b9e3eb68f775acfb7dc903f15e2c1
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-11 22:19:51 +0530

    Update FindUnique.java

--- Bit Manipulation/FindUnique.java
+++ Bit Manipulation/FindUnique.java
@@ -10,34 +10,36 @@ function findUnique(arr) {
 
 // Find two unique numbers from an array (every other number occurs twice)
 
-public static int[] find2Unique(int[] arr) {
-    int diff = 0;
-    int oneNo = 0;
-    int[] ans = new int[2];
-    for (int i = 0; i < arr.length; i++) {
-        diff = diff ^ arr[i];                  // this will give the diff btw the 2 unique num
+function find2Unique(arr) {
+    let diff = 0;
+    let oneNo = 0;
+    let ans = [];
+    for (let i = 0; i < arr.length; i++) {
+        diff = diff ^ arr[i]; // this will give the diff btw the 2 unique num
     }
-    for (int i = 0; i < arr.length; i++) {
-        if ((arr[i] & diff) > 0)               // we'll check all the numbers that have the diff
-            oneNo = oneNo ^ arr[i];            // and xor them to find one of the unique numbers
+    for (let i = 0; i < arr.length; i++) {
+        if ((arr[i] & diff) > 0)
+            // we'll check all the numbers that have the diff
+            oneNo = oneNo ^ arr[i]; // and xor them to find one of the unique numbers
     }
-    arr[0] = oneNo; 
-    arr[1] = oneNo ^ diff;                     // by applying xor to the number and the diff we get the second unique number
-    return arr;
+    ans[1] = oneNo;
+    ans[0] = oneNo ^ diff; // by applying xor to the number and the diff we get the second unique number
+    return ans;
 }
 
 // Find one unique numbers from an array (every other number occurs thrice)
 
-public static int findUnique3(int[] arr) {
-    int ans = 0;
-    for (int i = 0; i < 64; i++) {                   // we count the no of set bits in all 64 places
-        int count = 0;
-        for (int j = 0; j < arr.length; j++) {
-            if ((arr[j] & 1 << i) > 0)
-                count++;
+function findUnique3(arr) {
+    let ans = 0;
+    for (let i = 0; i < 64; i++) {
+        // we count the no of set bits in all 64 places
+        let count = 0;
+        for (let j = 0; j < arr.length; j++) {
+            if ((arr[j] & (1 << i)) > 0) count++;
         }
-        if (count % 3 != 0) {                        // the count will be divisible by 3 if the number is repeated thrice
-            ans = ans | 1 << i;
+        if (count % 3 != 0) {
+            // the count will be divisible by 3 if the number is repeated thrice
+            ans = ans | (1 << i);
         }
     }
     return ans;
