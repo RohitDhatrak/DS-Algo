9a372a74fa6327be4fede431c7ac673a01613edb
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-11 22:43:53 +0530

    Update SubSet.js

--- Bit Manipulation/SubSet.js
+++ Bit Manipulation/SubSet.js
@@ -1,8 +1,8 @@
 // print all the possible subsets of the array
 
-function subSets(arr, n) {
-    for (let i = 0; i < Math.pow(2, n); i++) {
-        for (let j = 0; j < n; j++) {
+function subSets(arr) {
+    for (let i = 0; i < Math.pow(2, arr.length); i++) {
+        for (let j = 0; j < arr.length; j++) {
             if ((i & (1 << j)) > 0) {
                 process.stdout.write(arr[j] + " ");
             }
