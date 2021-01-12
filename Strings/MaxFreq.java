f17beac07cde253b6f6e70a14fcfd839f8b4670c
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-12 23:47:50 +0530

    Update MaxFreq.java

--- Strings/MaxFreq.java
+++ Strings/MaxFreq.java
@@ -3,20 +3,31 @@
 // Time Complexity  O(n)
 // Space Complexity O(1)
 
-public static char maxFreq(String str) {
-    int[] count = new int[26];
-    for (int i = 0; i < str.length(); i++) {
-        if (str.charAt(i) >= 'a')
-            count[str.charAt(i) - 'a']++;
-        if (str.charAt(i) < 'a')
-            count[str.charAt(i) - 'A']++;
+// supporting function
+
+function toLower(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) >= "A" && str.charAt(i) <= "Z")
+            result += String.fromCharCode(str.charCodeAt(i) + 32);
+        else result += str.charAt(i);
     }
-    int max = 0;
-    char ans = 'a';
-    for (int i = 0; i < count.length; i++) {
+    return result;
+}
+
+function maxFreq(str) {
+    let count = [];
+    for (let i = 0; i < 26; i++) count[i] = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) >= "a") count[str.charCodeAt(i) - 97]++;
+        if (str.charAt(i) < "a") count[str.charCodeAt(i) - 65]++;
+    }
+    let max = 0;
+    let ans = "";
+    for (let i = 0; i < count.length; i++) {
         if (count[i] > max) {
             max = count[i];
-            ans = (char)(i + 'a');
+            ans = String.fromCharCode(i + 97);
         }
     }
     return ans;
