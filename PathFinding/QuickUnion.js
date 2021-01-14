b1e290ca9ffbc9b0016ac0cf82c0f2ccce027b2a
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-15 00:19:08 +0530

    Update QuickUnion.js

--- PathFinding/QuickUnion.js
+++ PathFinding/QuickUnion.js
@@ -2,20 +2,23 @@ class QuickUnion {
     constructor(n) {
         this.arr = [];
         this.n = n;
-        for (let i = 0; i < this.n; i++) this.arr[i - 1] = i;
+        for (let i = 0; i < this.n; i++) this.arr[i] = i;
+    }
+
+    root(node) {
+        while (this.arr[node] != node) {
+            node = this.arr[node];
+        }
+        return node;
     }
 
     union(child, head) {
-        while (this.arr[child] != child) {
-            child = this.arr[child];
-        }
-        while (this.arr[head] != head) {
-            head = this.arr[head];
-        }
+        child = root(child);
+        head = root(head);
         this.arr[child] = head;
     }
 
     connected(child, head) {
-        return this.arr[child] == this.arr[head];
+        return this.root(child) == this.root(head);
     }
 }
