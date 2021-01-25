d5c6dfb59f173d01d5726d0d50f07b84c100a50e
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-25 23:34:52 +0530

    Update RemoveElement.js

--- Arrays/RemoveElement.js
+++ Arrays/RemoveElement.js
@@ -3,22 +3,13 @@
 // Time Complexity  O(n^2)
 // Space Complexity O(1)
 
-var removeElement = function(nums, val) {
-    let length = 0;
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] == val){
-            for(let j = i + 1; j < nums.length; j++){
-                if(nums[j]!=val){
-                    let temp  = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    length++;
-                    break;
-                }
-            }
+var removeElement = function (nums, val) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++){
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
         }
-        else
-            length++;
     }
-    return length;
-}
+    return i;
+};
