090c45e3c136c7bad25f4721c657af4d621ebc16
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-25 18:35:35 +0530

    Update Kadanes.js

--- Arrays/Kadanes.js
+++ Arrays/Kadanes.js
@@ -1,6 +1,6 @@
 // Kadane's algorithm - used to find the maximum subarray sum
 // at every iteration we choose the greater number between arr[i] and currentSum + arr[i]
-// from the above statement we can conclude that whenever the currentSum becomes less than 0 we'll reset the currentSum 
+// from the above statement we can conclude that whenever the currentSum becomes -ve we'll reset the currentSum 
 // because arr[i] will always be greater than currentSum + arr[i] if currentSum is negative
 
 // Time Complexity O(n)
