dd837ddc189d5f3e31a320f4ca4f0ba82723d802
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-26 10:23:21 +0530

    Create MergeSortedArray.js

---
+++ Arrays/MergeSortedArray.js
@@ -0,0 +1,18 @@
+// 88. Merge Sorted Array
+
+// Time Complexity  O(m + n)
+// Space Complexity O(m + n)
+
+var merge = function(nums1, m, nums2, n) {
+    let ans = [];
+    let ansLen = 0;
+    let i = 0;
+    let j = 0;
+    while (i < m && j < n) {
+        if (nums1[i] <= nums2[j]) ans[ansLen++] = nums1[i++];
+        else ans[ansLen++] = nums2[j++];
+    }
+    if (i < m) for (let k = i; k < m; k++) ans[ansLen++] = nums1[k];
+    if (j < n) for (let k = j; k < n; k++) ans[ansLen++] = nums2[k];
+    for (let k = 0; k < ans.length; k++) nums1[k] = ans[k];
+};
