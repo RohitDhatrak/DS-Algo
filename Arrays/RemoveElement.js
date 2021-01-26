f1cedb5dfa754f2f49ea037dcc27b6e1ffa4f8cc
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-01-26 11:38:10 +0530

    Update RemoveElement.js

--- Arrays/RemoveElement.js
+++ Arrays/RemoveElement.js
@@ -1,15 +1,24 @@
 // Leetcode - 27. Remove Element
 
-// Time Complexity  O(n)
+// Time Complexity  O(n^2)
 // Space Complexity O(1)
 
-var removeElement = function (nums, val) {
-    let i = 0;
-    for (let j = 0; j < nums.length; j++){
-        if (nums[j] != val) {
-            nums[i] = nums[j];
-            i++;
+var removeElement = function(nums, val) {
+    let length = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] == val){
+            for(let j = i + 1; j < nums.length; j++){
+                if(nums[j]!=val){
+                    let temp  = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    length++;
+                    break;
+                }
+            }
         }
+        else
+            length++;
     }
-    return i;
+    return length;
 };
