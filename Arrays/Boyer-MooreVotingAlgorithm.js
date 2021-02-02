6da64d057f589bdb2749a7e4d71ca53831e0390e
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-02-02 08:54:59 +0530

    Update Boyer-MooreVotingAlgorithm.js

--- Arrays/Boyer-MooreVotingAlgorithm.js
+++ Arrays/Boyer-MooreVotingAlgorithm.js
@@ -1,3 +1,8 @@
+// This algorithm can be used to find a element that appears more than floor(n/2) times in an array
+// Because the majority element appears more than n/2 times in the array i.e a minimum of floor(n/2) + 1 times out of n
+// Therefore even if all the minority elements cancel out the majority elements one instance of majority element will be left in case of odd no of elements
+// and minimum two instances will be left in case of even no of elements
+
 // 169. Majority Element
 
 // Time Complexity  O(n)
