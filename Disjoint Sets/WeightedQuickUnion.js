7828361175687aec955af641fa7060c879c6d3fb
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-02-07 17:31:51 +0530

    Update WeightedQuickUnion.js

--- Disjoint Sets/WeightedQuickUnion.js
+++ Disjoint Sets/WeightedQuickUnion.js
@@ -1,28 +1,32 @@
 const _find = new WeakMap();
 const _N = new WeakMap();
-let _arr = new WeakMap();
+const _arr = new WeakMap();
 
 class WeightedQuickUnion {
     constructor(n) {
         if (n == undefined) throw new Error("Constructor parameter not passed");
 
         _arr.set(this, []);
-        let arr = _arr.get(this);
+        const arr = _arr.get(this);
         _N.set(this, n);
         for (let i = 0; i <= n; i++) arr[i] = -1;
+        this.count = 0;
 
         _find.set(this, (node) => {
             while (arr[node] >= 0) {
                 node = arr[node];
+                this.count += 2;
             }
+            this.count++;
             return node;
         });
     }
 
     union(a, b) {
-        let arr = _arr.get(this);
-        let find = _find.get(this);
-        let N = _N.get(this);
+        const arr = _arr.get(this);
+        const find = _find.get(this);
+        const N = _N.get(this);
+        this.count = 0;
 
         a = find(a);
         b = find(b);
@@ -34,16 +38,19 @@ class WeightedQuickUnion {
                 arr[b] += arr[a];
                 arr[a] = b;
             }
+            this.count += 6;
         }
         _N.set(this, N - 1);
+        console.log(arr);
+        console.log(this.count);
     }
 
     isConnected(a, b) {
-        let find = _find.get(this);
+        const find = _find.get(this);
         return find(a) == find(b);
     }
 
-    get count() {
+    get count1() {
         return _N.get(this);
     }
 }
