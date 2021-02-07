6955038bed3f1cee0d6b33f065ab47a8b2905a74
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-02-07 17:35:01 +0530

    Update QuickUnion.js

--- Disjoint Sets/QuickUnion.js
+++ Disjoint Sets/QuickUnion.js
@@ -1,13 +1,13 @@
 const _find = new WeakMap();
 const _N = new WeakMap();
-let _arr = new WeakMap();
+const _arr = new WeakMap();
 
 class QuickUnion {
     constructor(n) {
         if (n == undefined) throw new Error("Constructor parameter not passed");
 
         _arr.set(this, []);
-        let arr = _arr.get(this);
+        const arr = _arr.get(this);
         _N.set(this, n);
         for (let i = 0; i <= n; i++) arr[i] = i;
 
@@ -20,18 +20,20 @@ class QuickUnion {
     }
 
     union(a, b) {
-        let arr = _arr.get(this);
-        let find = _find.get(this);
-        let N = _N.get(this);
+        const arr = _arr.get(this);
+        const find = _find.get(this);
+        const N = _N.get(this);
 
         a = find(a);
         b = find(b);
-        if (a != b) arr[a] = b;
+        if (a != b) {
+            arr[a] = b;
+        }
         _N.set(this, N - 1);
     }
 
     isConnected(a, b) {
-        let find = _find.get(this);
+        const find = _find.get(this);
         return find(a) == find(b);
     }
 
