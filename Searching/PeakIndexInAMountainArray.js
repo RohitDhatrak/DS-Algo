a6f294eabddd1eff826b84a9d29c1a3243c4b375
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-05-19 11:30:58 +0530

    Create PeakIndexInAMountainArray.js

---
+++ Searching/PeakIndexInAMountainArray.js
@@ -0,0 +1,19 @@
+// 852. Peak Index in a Mountain Array
+
+// Time Complexity  O(logn)
+// Space Complexity O(1)
+
+var peakIndexInMountainArray = function(arr) {
+    let lhs = 0;
+    let rhs = arr.length - 1;
+    while(true){
+        const middle = Math.trunc((lhs + rhs) / 2);
+        if(arr[middle + 1] < arr[middle] && arr[middle - 1] < arr[middle]){
+            return middle;
+        }else if (arr[middle + 1] > arr[middle]){
+            lhs = middle + 1;
+        }else{
+            rhs = middle - 1;
+        }
+    }
+};
