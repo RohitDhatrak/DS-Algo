b41535f00fed53f3949860be63a86c01d904e025
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-06-16 12:18:53 +0530

    Update BubbleSort.js

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,17 +1,27 @@
 // In each iteration we compare the current element with the next element and swap them if they are in wrong order
+// Optimization: If there are no swaps in the last iteration the array is sorted. (Adaptive)
 
-// Time Complexity O(n^2)
-// Space Complexity O(1)
-// In-place, Stable, Adaptive, Not Online
+// Complexity          Best                        Average                    Worst
+
+// Time                O(n)                        O(n^2)                     O(n^2)
+
+// Space               O(1)                         O(1)                       O(1)
+
+// Parameter to consider while selectinga sorting algorithm
+// In Place, Stable, Adaptive, Not Online, Simple Implementation, Efficient for small datasets
 
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+        let isSorted = true;
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j + 1] < arr[j]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                isSorted = false;
             }
         }
+        if (isSorted) {
+            return arr;
+        }
     }
+    return arr;
 }
