a11203f08b592beb037e509dd02d59f2eb0518e6
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-06-18 12:26:16 +0530

    Update BubbleSort.js

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -7,7 +7,6 @@
 
 // Space               O(1)                         O(1)                       O(1)
 
-// Parameter to consider while selectinga sorting algorithm
 // Stable, Adaptive
 // Not Online
 
