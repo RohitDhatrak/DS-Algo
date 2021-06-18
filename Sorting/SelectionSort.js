6b92697803e7f1cc81fbeda5c79f19ffe41a075b
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-06-18 12:27:42 +0530

    Update SelectionSort.js

--- Sorting/SelectionSort.js
+++ Sorting/SelectionSort.js
@@ -5,7 +5,7 @@
 // Space Complexity O(1)
 
 // Not Stable, Not Adaptive, Not Online
-// We can make this algorithm stable by shifting all elements to the right and putting the min element in the empty slot.
+// We can make this algorithm stable by rotating all elements to the right and putting the min element in the empty slot.
 
 function selectionSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
