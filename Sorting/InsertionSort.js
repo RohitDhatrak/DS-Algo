3686fa3208b11ce69415bbdfc36abeccc9cf9d18
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-06-22 12:36:11 +0530

    Update InsertionSort.js

--- Sorting/InsertionSort.js
+++ Sorting/InsertionSort.js
@@ -1,18 +1,21 @@
 // We take an element from the unsorted part and insert it in the right place in the sorted part
 // Initially one element would be sorted because a single elment is always sorted in itself
 
-// Time Complexity O(n^2)
-// Space Complexity O(1)
+// Complexity          Best                        Average                    Worst
+
+// Time                O(n)                        O(n^2)                     O(n^2)
+
+// Space               O(1)                         O(1)                       O(1)
+
 // Stable, Adaptive, Online
 
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        while (arr[j] > current && j >= 0) {
-            arr[j + 1] = arr[j];
+        let j = i;
+        while (arr[j - 1] > arr[j] && j >= 0) {
+            [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
             j--;
         }
-        arr[j + 1] = current;
     }
+    return arr;
 }
