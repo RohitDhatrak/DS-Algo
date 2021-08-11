c175a45dae4165c460dff9b5634c98f16ceafd94
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-08-11 20:52:21 +0530

    Update RemoveDupicates.java

--- Arrays/RemoveDupicates.java
+++ Arrays/RemoveDupicates.java
@@ -1,3 +1,8 @@
+// Leetcode - 26. Remove Duplicates from Sorted Array
+
+// Time Complexity  O(n)
+// Space Complexity O(1)
+
 public static int removeDuplicates(int[] nums) {
     if (nums.length == 0) return 0;
     int lastPos = 0;
