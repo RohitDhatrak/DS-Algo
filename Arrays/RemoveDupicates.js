9e36d9106827b40241730049a2375476c1375374
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-08-11 20:46:55 +0530

    Update RemoveDupicates.js

--- Arrays/RemoveDupicates.js
+++ Arrays/RemoveDupicates.js
@@ -6,12 +6,10 @@
 var removeDuplicates = function(nums) {
     if (!nums.length) return 0;
     let lastPos = 0;
-    let length = 1;
     for (let i = 1; i < nums.length; i++) {
         if (nums[i] > nums[lastPos]) {
             nums[++lastPos] = nums[i];
-            length++
         }
     }
-    return length;
+    return lastPos + 1;
 };
