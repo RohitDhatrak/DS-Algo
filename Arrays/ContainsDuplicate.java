120cbdb114ac295b0ea120ae9a2a4b7f13c6365a
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-08-14 13:37:38 +0530

    Create ContainsDuplicate.java

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+// Return true if the array contains duplicates return false if it doesn't
+
+// Time Complexity  O(n)
+// Space Complexity O(n)
+
+public boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+    for (int i = 0; i < nums.length; i++) {
+        if (set.contains(nums[i])) {
+            return true;
+        } else {
+            set.add(nums[i]);
+        }
+    }
+    return false;
+}
