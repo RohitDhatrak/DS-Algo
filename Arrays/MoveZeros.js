086f2c52567e172b67eec7b358a5bda47e707a88
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-08-26 15:41:59 +0530

    Update MoveZeros.js

--- Arrays/MoveZeros.js
+++ Arrays/MoveZeros.js
@@ -4,14 +4,16 @@
 // Space Complexity O(1)
 
 var moveZeroes = function(nums) {
-   let slowPtr = 0;
-    let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] != 0) {
-            nums[slowPtr++] = nums[i];
-        } else count++;
+    let zeroCount = 0;
+    let slowPtr = 0;
+    for (let elem of nums) {
+        if (elem !== 0) {
+            nums[slowPtr++] = elem;
+        } else {
+            zeroCount++;
+        }
     }
-    for (let i = nums.length - 1; count > 0; i--, count--) {
+    for (let i = nums.length - 1; zeroCount > 0; i--, zeroCount--) {
         nums[i] = 0;
-    } 
+    }
 };
