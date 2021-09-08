e22510f72ccfcea4a35fe7a96bf19c9bb59d9c63
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-09-08 13:20:16 +0530

    Update MergeTwoSortedLists.js

--- LinkedList/MergeTwoSortedLists.js
+++ LinkedList/MergeTwoSortedLists.js
@@ -45,3 +45,22 @@ var mergeTwoLists = function (l1, l2) {
     }
     return mergedListHead;
 };
+
+// Cleaner version
+const mergedListHead = new ListNode(null, null);
+    let node = mergedListHead;
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            node.next = l1;
+            l1 = l1.next;
+        } else {
+            node.next = l2;
+            l2 = l2.next;
+        }
+        node = node.next;
+    }
+    node.next = l1 || l2;
+
+    return mergedListHead.next;
+};
