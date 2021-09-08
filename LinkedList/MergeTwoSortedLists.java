89b823b5f575f4c293b1fea3a04d6d27a6234787
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-09-08 16:13:09 +0530

    Update MergeTwoSortedLists.java

--- LinkedList/MergeTwoSortedLists.java
+++ LinkedList/MergeTwoSortedLists.java
@@ -3,47 +3,68 @@
 // Time Complexity  O(n + m)
 // Space Complexity O(1)
 
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null || l2 == null) {
-            if (l1 == null && l2 != null) {
-                return l2;
-            } else if (l1 != null && l2 == null) {
-                return l1;
-            } else {
-                return null;
-            }
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    if (l1 == null || l2 == null) {
+        if (l1 == null && l2 != null) {
+            return l2;
+        } else if (l1 != null && l2 == null) {
+            return l1;
+        } else {
+            return null;
         }
-        
-        ListNode mergedListHead;
-        ListNode node;
+    }
 
+    ListNode mergedListHead;
+    ListNode node;
+
+    if (l1.val < l2.val) {
+        mergedListHead = l1;
+        node = l1;
+        l1 = l1.next;
+    } else {
+        mergedListHead = l2;
+        node = l2;
+        l2 = l2.next;
+    }
+
+    while (l1 != null && l2 != null) {
         if (l1.val < l2.val) {
-            mergedListHead = l1;
-            node = l1;
+            node.next = l1;
             l1 = l1.next;
         } else {
-            mergedListHead = l2;
-            node = l2;
+            node.next = l2;
             l2 = l2.next;
         }
-
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                node.next = l1;
-                l1 = l1.next;
-            } else {
-                node.next = l2;
-                l2 = l2.next;
-            }
-            node = node.next;
-        }
-
-        if (l1 == null) {
-            node.next = l2;
-        } else {
-            node.next = l1;
-        }
-        return mergedListHead;
+        node = node.next;
     }
+
+    if (l1 == null) {
+        node.next = l2;
+    } else {
+        node.next = l1;
+    }
+    return mergedListHead;
+}
+
+// Cleaner solution
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    ListNode mergedListHead = new ListNode(0, null);
+    ListNode node = mergedListHead;
+
+    while (l1 != null && l2 != null) {
+        if (l1.val < l2.val) {
+            node.next = l1;
+            l1 = l1.next;
+        } else {
+            node.next = l2;
+            l2 = l2.next;
+        }
+        node = node.next;
+    }
+    if (l1 == null) {
+        node.next = l2;
+    } else if (l2 == null){
+        node.next = l1;
+    }
+    return mergedListHead.next;
 }
