c33e9ab1459a7a0fb0c5ce6b6f2fc95be821c645
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-09-10 19:17:30 +0530

    Create FirstBadVersion.java

---
+++ Searching/FirstBadVersion.java
@@ -0,0 +1,20 @@
+// 278. First Bad Version
+
+// Time Complexity  O(logn)
+// Space Complexity O(1)
+
+public int firstBadVersion(int n) {
+    int start = 1;
+    int end = n;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2; // Overflow issues
+        if (isBadVersion(mid)) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    return end + 1;
+}
