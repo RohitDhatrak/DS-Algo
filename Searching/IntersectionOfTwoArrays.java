acac26252701a2613830d604b2be032a6ed05ca4
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-09-10 18:05:39 +0530

    Create IntersectionOfTwoArrays.java

---
+++ Searching/IntersectionOfTwoArrays.java
@@ -0,0 +1,49 @@
+// 349. Intersection of Two Arrays
+
+// Time Complextiy  O(logn) [assuming arrays are sorted]
+// Space Complexity O(1)
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int[] smaller = nums1.length <= nums2.length ? nums1 : nums2;
+        int[] larger = nums1.length > nums2.length ? nums1 : nums2;
+        int lastNum = -1;
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int num: smaller) {
+            if (num != lastNum && binarySearch(larger, num) ) {
+                list.add(num);
+            }
+            lastNum = num;
+        }
+
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+
+        return ans;
+    }
+    
+    public boolean binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+        
+    }
+}
