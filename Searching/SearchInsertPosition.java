98366ff25e2dd9e3ec7f3efc83a02b058a377cc6
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-09-10 20:20:22 +0530

    Create SearchInsertPosition.java

---
+++ Searching/SearchInsertPosition.java
@@ -0,0 +1,21 @@
+// 35. Search Insert Position
+
+// Time Complexity  O(logn)
+// Space Complexity O(1)
+
+public int searchInsert(int[] nums, int target) {
+    int start = 0;
+    int end = nums.length - 1;
+
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return end+1;
+}
