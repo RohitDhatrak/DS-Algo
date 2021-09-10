7390fc7ca171d3641d7ca07e3f19f6ffaf2e3881
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-09-10 20:25:39 +0530

    Update SearchInsertPosition.js

--- Searching/SearchInsertPosition.js
+++ Searching/SearchInsertPosition.js
@@ -7,7 +7,7 @@ var searchInsert = function(nums, target) {
     let start = 0;
     let end = nums.length - 1;
 
-    while (start <= end) {
+    while (start <= end) {  // = is necessary
         const mid = Math.floor((start + end) / 2);
         if (nums[mid] === target) {
             return mid;
@@ -17,5 +17,5 @@ var searchInsert = function(nums, target) {
             start = mid + 1;
         }
     }
-    return end+1;
+    return end+1;  // or start
 };
