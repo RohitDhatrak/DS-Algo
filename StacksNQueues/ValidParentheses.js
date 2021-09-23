169de0edff7c270875538a13b69b9a56132cfa75
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-09-23 20:37:49 +0530

    Update ValidParentheses.js

--- StacksNQueues/ValidParentheses.js
+++ StacksNQueues/ValidParentheses.js
@@ -1,1 +1,32 @@
 // 20. Valid Parentheses
+
+// Time Complexity  O(n)
+// Space Complexity O(n)
+
+var isValid = function (s) {
+    if (s.length % 2 !== 0) {
+        return false;
+    }
+
+    const map = {
+        "{": "}",
+        "[": "]",
+        "(": ")",
+    };
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (map[s[i]]) {
+            stack.push(s[i]);
+        } else {
+            const lastElement = stack[stack.length - 1];
+            if (s[i] === map[lastElement]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
