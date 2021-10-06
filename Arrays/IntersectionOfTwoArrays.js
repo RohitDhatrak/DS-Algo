b84386a89facdb5917b10f3ec666b3724b85111f
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-10-06 18:55:51 +0530

    Create IntersectionOfTwoArrays.js

---
+++ Arrays/IntersectionOfTwoArrays.js
@@ -0,0 +1,21 @@
+// 349. Intersection of Two Arrays
+
+// Time Complexity:  O(n)
+// Space Complexity: O(n)
+
+var intersection = function (nums1, nums2) {
+    const map = new Map();
+    const output = [];
+    for (const num of nums1) {
+        if (!map.has(num)) {
+            map.set(num, 1);
+        }
+    }
+    for (const num of nums2) {
+        if (map.has(num)) {
+            output.push(num);
+            map.delete(num);
+        }
+    }
+    return output;
+};
