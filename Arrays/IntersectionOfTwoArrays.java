00868a5bd20cf35d4edbf1f9c1aed157312bf5e6
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-10-06 19:04:18 +0530

    Create IntersectionOfTwoArrays.java

---
+++ Arrays/IntersectionOfTwoArrays.java
@@ -0,0 +1,26 @@
+// 349. Intersection of Two Arrays
+
+// Time Complexity:  O(n)
+// Space Complexity: O(n)
+
+public int[] intersection(int[] nums1, int[] nums2) {
+    HashSet<Integer> map = new HashSet<>();
+    ArrayList<Integer> list = new ArrayList<>();
+    for (int num : nums1) {
+        if (!map.contains(num)) {
+            map.add(num);
+        }
+    }
+    for (int num : nums2) {
+        if (map.contains(num)) {
+            list.add(num); 
+            map.remove(num);
+        }
+    }
+
+    int[] output = new int[list.size()];
+    for (int i = 0; i < list.size(); i++) {
+        output[i] = list.get(i);
+    }
+    return output;
+}
