04152313234cd2d55a8656d74002ac12a098e8e7
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-10-07 20:45:26 +0530

    Create LongPressedName.java

---
+++ TwoPointers/LongPressedName.java
@@ -0,0 +1,24 @@
+// 925. Long Pressed Name
+
+// Time Complexity  O(n+m)
+// Space Complexity O(1)
+
+
+public boolean isLongPressedName(String name, String typed) {
+    if (typed.length() < name.length()) return false;
+    int namePtr = 0;
+    int typedPtr = 0;
+
+    while (typedPtr < typed.length() || namePtr < name.length()) {
+        if(typedPtr >= typed.length()) return false;
+        if (namePtr < name.length() && name.charAt(namePtr) == typed.charAt(typedPtr)) {
+            namePtr++;
+            typedPtr++;
+        } else if (namePtr != 0 && name.charAt(namePtr - 1) == typed.charAt(typedPtr)) {
+            typedPtr++;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
