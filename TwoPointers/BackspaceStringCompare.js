48b29f368724be83d9ab22d227ad6ff4595f398e
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-10-07 18:43:09 +0530

    Create BackspaceStringCompare.js

---
+++ TwoPointers/BackspaceStringCompare.js
@@ -0,0 +1,53 @@
+// 844. Backspace String Compare
+
+// Time Complexity  O(n)
+// Space Complexity O(1)
+
+var backspaceCompare = function (s, t) {
+    let ptr1 = s.length - 1;
+    let ptr2 = t.length - 1;
+    let sBackSpaceCount = 0;
+    let tBackSpaceCount = 0;
+
+    while (ptr1 >= 0 || ptr2 >= 0) {
+        if (sBackSpaceCount === 0 && tBackSpaceCount === 0) {
+            if (s[ptr1] !== "#" && t[ptr2] !== "#") {
+                if (s[ptr1] !== t[ptr2]) {
+                    return false;
+                } else {
+                    ptr1--;
+                    ptr2--;
+                }
+            } else {
+                if (s[ptr1] === "#") {
+                    sBackSpaceCount++;
+                    ptr1--;
+                }
+                if (t[ptr2] === "#") {
+                    tBackSpaceCount++;
+                    ptr2--;
+                }
+            }
+        } else {
+            while (sBackSpaceCount !== 0) {
+                if (s[ptr1] === "#") {
+                    sBackSpaceCount++;
+                    ptr1--;
+                } else {
+                    sBackSpaceCount--;
+                    ptr1--;
+                }
+            }
+            while (tBackSpaceCount !== 0) {
+                if (t[ptr2] === "#") {
+                    tBackSpaceCount++;
+                    ptr2--;
+                } else {
+                    tBackSpaceCount--;
+                    ptr2--;
+                }
+            }
+        }
+    }
+    return true;
+};
