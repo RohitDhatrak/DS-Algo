73acd84508846fe5eca4e61e995aa9a03b3fdf09
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-10-09 08:26:15 +0530

    Update FruitIntoBaskets.js

--- Sliding Window/FruitIntoBaskets.js
+++ Sliding Window/FruitIntoBaskets.js
@@ -3,25 +3,25 @@
 // Time Complexity  O(n)
 // Space Complexity O(1)
 
-var totalFruit = function(fruits) {
+var totalFruit = function (fruits) {
     let maxFruits = 0;
-    let slowPtr = 0;
-    let fastPtr = 0;
+    let lastFruit = 0;
+    let startPoint = 0;
     let count = 0;
     let types = [fruits[0], fruits[0]];
 
-    while (fastPtr < fruits.length) {
-        let currentFruit = fruits[fastPtr];
+    while (startPoint < fruits.length) {
+        let currentFruit = fruits[startPoint];
         if (currentFruit === types[0] || currentFruit === types[1]) {
             count++;
-            fastPtr++;
+            startPoint++;
         } else if (types[0] === types[1]) {
             types[1] = currentFruit;
-            slowPtr = fastPtr;
+            lastFruit = startPoint;
             count++;
-            fastPtr++;
+            startPoint++;
         } else {
-            fastPtr = slowPtr;
+            startPoint = lastFruit;
             types[0] = types[1];
             count = 0;
         }
