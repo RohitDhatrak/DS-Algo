1ef184a5c5e9fd8f199ed28cff7ac80f098e29db
Rohit Dhatrak <rdhatrak19@gmail.com>
2021-10-10 18:36:43 +0530

    Create FruitIntoBaskets.js

---
+++ TwoPointers/FruitIntoBaskets.js
@@ -0,0 +1,35 @@
+// 904. Fruit Into Baskets
+
+// Time Complexity  O(n)
+// Space Complexity O(1)
+
+// Solution 1: two pointer approach
+var totalFruit = function (fruits) {
+    let maxFruits = 0;
+    let lastFruit = 0;
+    let startPoint = 0;
+    let count = 0;
+    let types = [fruits[0], fruits[0]];
+
+    while (startPoint < fruits.length) {
+        let currentFruit = fruits[startPoint];
+        if (currentFruit === types[0] || currentFruit === types[1]) {
+            count++;
+            startPoint++;
+        } else if (types[0] === types[1]) {
+            types[1] = currentFruit;
+            lastFruit = startPoint;
+            count++;
+            startPoint++;
+        } else {
+            startPoint = lastFruit;
+            types[0] = types[1];
+            count = 0;
+        }
+        maxFruits = Math.max(maxFruits, count);
+    }
+
+    return maxFruits;
+};
+
+// Solution 2: check sliding window folder
