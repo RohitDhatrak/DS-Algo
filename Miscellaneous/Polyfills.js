c6153b45cfd91e9dc3acc8483d19502fd2999291
Rohit Dhatrak <rdhatrak19@gmail.com>
2022-04-05 08:13:24 +0530

    Update Polyfills.js

--- Miscellaneous/Polyfills.js
+++ Miscellaneous/Polyfills.js
@@ -52,3 +52,16 @@ Function.prototype.myBind = function myBind(thisArg, ...args) {
         fn.call(thisArg, ...args, ...args2);
     };
 };
+
+function memoize(func) {
+    const cache = {};
+    return function memoizedFunction(...args) {
+        const stringifiedArgs = JSON.stringify(args);
+        if (cache[stringifiedArgs]) {
+            return cache[stringifiedArgs];
+        }
+        const result = func(...args);
+        cache[stringifiedArgs] = result;
+        return result;
+    };
+}
